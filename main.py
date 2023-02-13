5e78daf1de69a5b77e35e8eb43bbe8693ff9f4d5
6ty2 <93207299+6ty2@users.noreply.github.com>
2023-02-13 15:16:35 +0200

    1.1

--- main.py
+++ main.py
@@ -9,18 +9,17 @@ Bracket = namedtuple("Bracket", ["char", "position"])
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
 
-
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next in enumerate(text):
+    for I, next in enumerate(text):
         if next in "([{":
-            opening_brackets_stack.append(Bracket(next, i+1))
+            opening_brackets_stack.append(Bracket(next, I+1))
         
         if next in ")]}":
             if not opening_brackets_stack:
-                return i+1
+                return I+1
             if not are_matching(opening_brackets_stack[-1].char, next):
-                return i+1
+                return I+1
             opening_brackets_stack.pop()
     if opening_brackets_stack:
         return opening_brackets_stack[0].position
